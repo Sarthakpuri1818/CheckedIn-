085158b9cbbd9daea208caa0493a6d22fc1ecdc6
Sarthakpuri1818 <sarthakpuri786@gmail.com>
2026-03-23 14:34:53 -0700

    added logout functionality

--- checkedin/components/navbar.tsx
+++ checkedin/components/navbar.tsx
@@ -1,9 +1,12 @@
+"use client";
+
+
 //navbar component is created to be added in the layout so that it can be used in all the pages
 //it has my logo and all the links to the pages in the app
 
 
 import Link from "next/link";
-
+import { useState, useEffect } from "react";
 import "./navbar.css";
 
 
@@ -11,6 +14,37 @@ import "./navbar.css";
 
 
 export default function Navbar(){
+//creating a function where it hides the navbar when user logs in to dashboard
+
+const [isLoggedIn, setIsLoggedIn]= useState(false);
+useEffect(()=>{
+    const loggedIn = document.cookie.includes("staffUser")||
+    document.cookie.includes("token");
+    setIsLoggedIn(loggedIn);
+
+},[])
+
+const handleLogout = async()=>{
+    try{
+        await fetch ("/api/logout",{
+            method:"POST",
+        });
+        setIsLoggedIn(false);
+        window.location.href="/";
+    }
+    catch(error){
+        console.error("Logout failed", error);
+
+    
+
+        
+    }
+}
+
+
+
+
+
     return(
         <nav className="navbar">
             <div className="logo-container">
@@ -20,8 +54,26 @@ export default function Navbar(){
             </div>
             <div className="nav-links">
                 <Link href="/" >Home</Link>
-                <Link href="/register">Register</Link>
-                  <Link href="/staff">Login</Link>
+
+
+                {!isLoggedIn && (
+                    <>
+                      <Link href="/register">Register</Link>
+                      <Link href="/staff">Login</Link>
+                    </>
+                  
+
+                )}
+
+                {isLoggedIn && (
+                    <>
+                        <Link href="/dashboard/staff">Dashboard</Link>
+                        <button className="logout-button" onClick={handleLogout}>Logout</button>
+                    </>
+                )}
+
+
+                
                   {/* <Link href="/aboutus">About Us</Link> */}
 
   
